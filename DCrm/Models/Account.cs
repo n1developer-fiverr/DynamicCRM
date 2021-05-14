e6cd37445d704888c7c68e8e4da023d2e27afa65
n1developer-ubt <53445531+n1developer-ubt@users.noreply.github.com>
2021-05-14 15:29:13 +0500

    Models added

---
+++ DCrm/Models/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DCrm.Models
+{
+    public class Account:INotifyPropertyChanged
+    {
+        private string _columnBreak;
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Id { get; set; }
+        public string OwnerId { get; set; }
+        public string ColumnBreak { get=>_columnBreak; set {
+                if (value == null)
+                    return;
+
+                _columnBreak = value;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ColumnBreak)));
+            }
+        }
+        public bool AllowUpdate { get; set; } = true;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
